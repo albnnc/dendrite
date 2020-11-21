fd3c2f64b69805abd2515e0f4730cf7d46937611
a.kosyrev <a.kosyrev@deltasolutions.ru>
2020-11-21 19:41:55 +0300

    Fix rendering

--- src/app_start.cpp
+++ src/app_start.cpp
@@ -7,22 +7,22 @@ void App::start() {
     pollEvents();
 
     if (mayIterate) {
+      auto begin = std::chrono::system_clock::now();
       for (int i = 0; i < stepsPerFrame; ++i) {
-        auto begin = std::chrono::system_clock::now();
         field.cycle();
-        if (logLevel >= 3) {
-          auto end = std::chrono::system_clock::now();
-          std::chrono::duration<double> diff = end - begin;
-          double seconds = diff.count();
-          std::cout << "Computed "
-                    << stepsPerFrame
-                    << " steps during "
-                    << seconds
-                    << " s ("
-                    << stepsPerFrame / seconds
-                    << " steps/s)"
-                    << std::endl;
-        }
+      }
+      if (logLevel >= 3) {
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> diff = end - begin;
+        double seconds = diff.count();
+        std::cout << "Computed "
+                  << stepsPerFrame
+                  << " steps during "
+                  << seconds
+                  << " s ("
+                  << stepsPerFrame / seconds
+                  << " steps/s)"
+                  << std::endl;
       }
       if (mayUpdateDimensionDataFiles &&
           (dimensionDataFilesUpdateStep == -1 ||
